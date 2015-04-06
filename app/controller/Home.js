3c1e785f64405c028e435ee907de833e8f4206f5
Shweta <Shweta@lawn-128-61-60-243.lawn.gatech.edu>
2015-04-06 19:47:17 -0400

    disabled forms for other users

--- app/controller/Home.js
+++ app/controller/Home.js
@@ -38,13 +38,29 @@ Ext.define('MicroEvents.controller.Home', {
 
         console.log(index, record)
         console.log(record.data.owner_id)
-       // if (owner_id == record.data.owner_id){
+        console.log(localStorage.getItem("MicroEvents_user_id"))
+
+        if (localStorage.getItem("MicroEvents_user_id") == record.data.owner_id)
+        {
+
         Ext.getCmp('eventEditor_title').setValue(record.data.title)
         Ext.getCmp('eventEditor_venue').setValue(record.data.venue)
         Ext.getCmp('eventEditor_date').setValue(record.data.date)
         Ext.getCmp('eventTime').setValue(record.data.time)
-    //}
         this.showAddEventForm()
+    }
+    else{
+
+        Ext.getCmp('eventEditor_title').setValue(record.data.title).disable()
+        Ext.getCmp('eventEditor_venue').setValue(record.data.venue).disable()
+        Ext.getCmp('eventEditor_date').setValue(record.data.date).disable()
+        Ext.getCmp('eventTime').setValue(record.data.time).disable()
+
+        this.showAddEventForm()
+
+    }
+
+        
 
     },
     
